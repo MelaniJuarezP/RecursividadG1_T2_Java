b4888a06ffc999e437c7cd35e400c0cf36ff962b
MelaniJuarezP <25030133@itesa.edu.mx>
2026-09-13 18:36:24 -0600

    Versión_02 (recursividades)

--- Recursividad/src/recursividad/Recursividad.java
+++ Recursividad/src/recursividad/Recursividad.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package recursividad;
-
 /**
- *
- * @author Melani
+ * @author Melani Lizbeth Juarez Pacheco
  */
 public class Recursividad {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Recursividades oRecursividad = new Recursividades();
+        
+        System.out.println("Sumar vocales");
+        String cadena = "ParangaricutirimicUARO";
+        int contar= oRecursividad.vocales(cadena);
+        System.out.println("Numero de vocales en '" + cadena + "' es : " + contar);
+        
+        System.out.println("\nSumar digitos");
+        int[] numeros = {123, 4567, 99,14};
+        int maxNumero = 0;
+        int maxSuma = -1;        
+        for (int num : numeros) {
+            int sumaActual = oRecursividad.sumaDigitos(num);
+            System.out.println("Número: " + num + ", Suma de sus dígitos: " + sumaActual);
+
+            if (sumaActual > maxSuma) {
+                maxSuma = sumaActual;
+                maxNumero = num;
+            }
+        }
+        System.out.println("\nEl entero con la mayor suma de dígitos es: " + maxNumero);
     
+        System.out.println("\nSuma de los primeros digitos");
+        int num=5;
+        int n = oRecursividad.sumaNumeros(num);
+        System.out.println("La suma de los primeros " + num + " números es: " + n);
+    }
 }
